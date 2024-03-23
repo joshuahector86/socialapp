16b9239e65a0627e6856aae88655a232eed9f49f
Dimitri Hector <joshuahector86@gmail.com>
2024-03-23 16:13:19 -0400

    fixing theme issues, updating sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,13 +1,21 @@
 import { sidebarUrls } from "@/utils/urls";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const currentLink = (path: string): boolean => {
+    return location.pathname === path;
+  };
   return (
     <div className="p-8 flex flex-col ">
       {sidebarUrls.map((link) => (
         <Link
+          key={link.name}
           to={link.href}
-          className="flex gap-2 items-center mb-8 p-2 text-xl hover:bg-slate-500 rounded-[1rem]"
+          className={`flex gap-2 items-center mb-4 p-2 text-xl 
+          hover:bg-accent rounded-[1rem] min-w-[15rem] ${
+            currentLink(link.href) ? "bg-accent" : ""
+          }`}
         >
           <link.icon className="w-[1.2rem] h-[1.2rem]" />
           {link.name}
